e85afc89fa7993ea0dfdd37d6e70df450658151e
ngthhoangkim <nguyenthihoangkim07022004@gmail.com>
2024-12-19 21:09:33 +0700

    create project

---
+++ admin/src/App.js
@@ -0,0 +1,9 @@
+import './css/App.css';
+
+function App() {
+  return (
+    <p>ADMIN</p>
+  );
+}
+
+export default App;
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import './css/App.css';
+
+function App() {
+  return (
+    <p>client</p>
+  );
+}
+
+export default App;
